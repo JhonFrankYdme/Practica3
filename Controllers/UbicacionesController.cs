a35b4db5c6ef86fa73ef57ebcd16b549f46aea1c
JhonFrankYdme <jhonfrankdymebenito@gmail.com>
2021-05-21 21:39:33 -0500

    PC3

--- Controllers/UbicacionesController.cs
+++ Controllers/UbicacionesController.cs
@@ -9,14 +9,13 @@ namespace pokemon001.Controllers
 {
     public class UbicacionesController : Controller
     {
-
         private readonly PokemonContext _context;
         public UbicacionesController(PokemonContext context) {
             _context = context;
         }
 
         public IActionResult Regiones() {
-            var regiones = _context.Regiones.OrderBy(r => r.Nombre).ToList();
+            var regiones = _context.Regiones.Include(x => x.Pueblos).OrderBy(r => r.Nombre).ToList();
             return View(regiones);
         }
 
@@ -24,17 +23,16 @@ namespace pokemon001.Controllers
             var pueblos = _context.Pueblos.Include(x => x.Region).OrderBy(r => r.Nombre).ToList();
             return View(pueblos);
         }
+
         public IActionResult Entrenadores() {
             var entrenadores = _context.Entrenadores.Include(x => x.Pueblo).OrderBy(r => r.Nombre).ToList();
             return View(entrenadores);
         }
 
-
         public IActionResult NuevoPueblo() {
             ViewBag.Regiones = _context.Regiones.ToList().Select(r => new SelectListItem(r.Nombre, r.Id.ToString()));
             return View();
         }
-       
 
         [HttpPost]
         public IActionResult NuevoPueblo(Pueblo r) {
@@ -77,29 +75,16 @@ namespace pokemon001.Controllers
             return RedirectToAction("Regiones");
         }
 
-        public IActionResult EditarRegion(int id) {
-            var region = _context.Regiones.Find(id);
-            return View(region);
+        public IActionResult BorrarPueblo(int id) {
+            var pueblo = _context.Pueblos.Find(id);
+            _context.Remove(pueblo);
+            _context.SaveChanges();
+
+            return RedirectToAction("Pueblos");
         }
 
 
-         public IActionResult NuevoEntrenador() {
-            ViewBag.Pueblos = _context.Pueblos.ToList().Select(r => new SelectListItem(r.Nombre, r.Id.ToString()));
-            return View();
-        }
-        [HttpPost]
-         public IActionResult NuevoEntrenador(Entrenador e) {
-         if (ModelState.IsValid) {
-                _context.Add(e);
-                _context.SaveChanges();
-                return RedirectToAction("NuevoEntrenadorConfirmacion");
-            }
-            return View(e);
-        }
-        public IActionResult NuevoEntrenadorConfirmacion() {
-            return View();
-        }
-        [HttpPost]
+
         public IActionResult BorrarEntrenador(int id) {
             var entrenador = _context.Entrenadores.Find(id);
             _context.Remove(entrenador);
@@ -108,13 +93,14 @@ namespace pokemon001.Controllers
             return RedirectToAction("Entrenadores");
         }
 
-        public IActionResult EditarEntrenador(int id) {
-            var entrenador = _context.Entrenadores.Find(id);
-            return View(entrenador);
-        }
-        
 
-        
+
+        public IActionResult EditarRegion(int id) {
+            var region = _context.Regiones.Find(id);
+            return View(region);
+        }
+
+        [HttpPost]
         public IActionResult EditarRegion(Region r) {
             if (ModelState.IsValid) {
                 var region = _context.Regiones.Find(r.Id);
@@ -128,5 +114,87 @@ namespace pokemon001.Controllers
         public IActionResult EditarRegionConfirmacion() {
             return View();
         }
+
+
+
+
+
+
+//--------------
+
+
+ public IActionResult EditarPueblo(int id) {
+            var pueblo = _context.Pueblos.Find(id);
+            return View(pueblo);
+        }
+
+        [HttpPost]
+        public IActionResult EditarPueblo(Pueblo r) {
+            if (ModelState.IsValid) {
+                var pueblo = _context.Pueblos.Find(r.Id);
+                pueblo.Nombre = r.Nombre;
+                _context.SaveChanges();
+                return RedirectToAction("EditarPuebloConfirmacion");
+            }
+            return View(r);
+        }
+
+        public IActionResult EditarPuebloConfirmacion() {
+            return View();
+        }
+
+
+
+
+
+        public IActionResult NuevoEntrenador() {
+            ViewBag.Pueblos = _context.Pueblos.ToList().Select(r => new SelectListItem(r.Nombre, r.Id.ToString()));
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NuevoEntrenador(Entrenador r) {
+            if (ModelState.IsValid) {
+                _context.Add(r);
+                _context.SaveChanges();
+                return RedirectToAction("NuevoEntrenadorConfirmacion");
+            }
+            return View(r);
+        }
+
+        public IActionResult NuevoEntrenadorConfirmacion() {
+            return View();
+        }
+
+
+
+public IActionResult EditarEntrenador(int id) {
+            var entrenador = _context.Entrenadores.Find(id);
+            return View(entrenador);
+        }
+
+        [HttpPost]
+        public IActionResult EditarEntrenador(Entrenador r) {
+            if (ModelState.IsValid) {
+                var entrenador = _context.Entrenadores.Find(r.Id);
+                entrenador.Nombre = r.Nombre;
+                _context.SaveChanges();
+                return RedirectToAction("EditarEntrenadorConfirmacion");
+            }
+            return View(r);
+        }
+
+        public IActionResult EditarEntrenadorConfirmacion() {
+            return View();
+        }
+
+
+
+
+
+
+
+
+        
     }
-    }
+}
